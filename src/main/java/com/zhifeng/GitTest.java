c5e0af0ee1c4d104c2ce6b0a92d13c4d104dd198
Aubrey <237915026@qq.com>
2022-07-31 17:40:55 +0800

    first commit

---
+++ src/main/java/com/zhifeng/GitTest.java
@@ -0,0 +1,12 @@
+package com.zhifeng;
+
+/**
+ * @Author:Aubrey
+ * @Version:1.0
+ * @Date:2022/7/31 17:22
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git!");
+    }
+}
