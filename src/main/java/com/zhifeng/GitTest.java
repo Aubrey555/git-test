7976f5067d10d0859bfd2dc51e164304915a9f44
Aubrey <237915026@qq.com>
2022-08-01 15:25:42 +0800

    hot-fix commit fourth

--- src/main/java/com/zhifeng/GitTest.java
+++ src/main/java/com/zhifeng/GitTest.java
@@ -9,8 +9,8 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git!");
         System.out.println("hello Git2!");
-        System.out.println("hello Git3!");
-        System.out.println("hello Git4!");
+        System.out.println("hello hot-fix Git3!");
+        System.out.println("hello hot-fix Git4!");
         System.out.println("Hot-Fix Test!");
     }
 }
